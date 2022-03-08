acedba1d33ac3f5eb8084f28d2a13424c77d76f1
sanjeev kumar <sanzeevkumar007@gmail.com>
2022-03-08 18:38:56 +0530

    super() and this()  use case

---
+++ src/Contructor/Combosuperthis.java
@@ -0,0 +1,25 @@
+package Contructor;
+
+public class Combosuperthis {
+   /* Combosuperthis()
+    {
+        System.out.println("construtor");
+        super();
+    }
+
+    */
+   /*
+    Combosuperthis()
+
+    {
+        super();
+        this();
+        System.out.println("simultaneusly");
+    }
+    */
+    public void Combosuperthis()
+    {
+        super();
+        System.out.println("method ");
+    }
+}

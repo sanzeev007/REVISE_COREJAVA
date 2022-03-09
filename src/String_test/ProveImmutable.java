1561c3b4e0842bb4d77e50f71ea9c83a8860a8be
sanjeev kumar <sanzeevkumar007@gmail.com>
2022-03-09 18:26:55 +0530

    immutable ok

--- src/String_test/ProveImmutable.java
+++ src/String_test/ProveImmutable.java
@@ -3,6 +3,7 @@ package String_test;
 public class ProveImmutable {
     public static void main(String[] args) {
         String s=new String("test");
+        String s2="hello";
         s.concat("string");
         System.out.println(s);
     }
